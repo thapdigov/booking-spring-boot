6e456ad2d4a31634de8514c0bb82643f8a20ae60
Behruz Tapdiqov <tapdigovbahruz@gmail.com>
2025-01-22 20:58:46 +0400

    First Commit

---
+++ src/main/java/az/turing/bookingspringboot/BookingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package az.turing.bookingspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/turing/bookingspringboot/BookingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package az.turing.bookingspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
